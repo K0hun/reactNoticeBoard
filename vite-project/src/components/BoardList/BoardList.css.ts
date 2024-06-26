9ba38b57f7f248ef6061db7848936eae5a41363a
Hun <dudgns9194@gmail.com>
2024-06-26 14:39:13 +0900

    Board List 생성하기

---
+++ vite-project/src/components/BoardList/BoardList.css.ts
@@ -0,0 +1,29 @@
+import { style } from "@vanilla-extract/css";
+
+export const container = style({
+
+})
+
+export const title = style({
+
+})
+
+export const addButton = style({
+
+})
+
+export const boardItem = style({
+
+})
+
+export const boardItemActive = style({
+
+})
+
+export const addSection = style({
+
+})
+
+export const smallTitle = style({
+    
+})
--- vite-project/src/components/BoardList/BoardList.tsx
+++ vite-project/src/components/BoardList/BoardList.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { FC, useState } from 'react'
+import { useTypedSelector } from '../../hooks/redux';
+import SideForm from './SideForm/SideForm';
+import {FiPlusCircle} from 'react-icons/fi';
+import { addButton, addSection, container, title } from './BoardList.css';
 
-const BoardList = () => {
+type TBoardListProps = {
+  activeBoardId: string;
+  setActiveBoardId: React.Dispatch<React.SetStateAction<string>>
+}
+
+const BoardList: FC<TBoardListProps> = ({
+  activeBoardId,
+  setActiveBoardId
+}) => {
+
+  const { boardArray } = useTypedSelector(state => state.boards);
+  const {isFormOpen, setIsFormOpen} = useState(false);
+
   return (
-    <div>BoardList</div>
+    <div className={container}>
+      <div className={title}>
+        게시판:
+      </div>
+      {boardArray.map((board, index) => (
+        <div key={board.boardId}>
+          <div>
+            {board.boardName}
+          </div>
+        </div>
+      ))}
+      <div className={addSection}>
+        {
+          isFormOpen ?
+            <SideForm setIsFormOpen={setIsFormOpen}/>
+            :
+            <FiPlusCircle className={addButton} onClick={() => setIsFormOpen(!isFormOpen)}/>
+        }
+      </div>
+    </div>
   )
 }
 
