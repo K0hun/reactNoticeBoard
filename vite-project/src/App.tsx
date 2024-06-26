94cc60e192c181ae41fc9c5a0d04b2fc4ea0b1bf
Hun <dudgns9194@gmail.com>
2024-06-26 15:19:37 +0900

    SideForm 생성하기

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+// import reactLogo from './assets/react.svg'
 import { appContainer, board, buttons } from './App.css'
 import BoardList from './components/BoardList/BoardList'
 
