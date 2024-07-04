f0baca316d2e62aa3e7ec52873a6d83a1170845a
Hun <dudgns9194@gmail.com>
2024-07-04 14:48:05 +0900

    Firebase 연결하기

---
+++ vite-project/src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyClHuX_In4aEJ8-SfJu4zvGzX5MRfFlNyo",
+  authDomain: "react-test-app-2-c3719.firebaseapp.com",
+  projectId: "react-test-app-2-c3719",
+  storageBucket: "react-test-app-2-c3719.appspot.com",
+  messagingSenderId: "1068785311220",
+  appId: "1:1068785311220:web:eeda536cdd953b91392da1"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
